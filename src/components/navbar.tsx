5e6f1135c887f9199e1628b97c32964aa6de65de
ottogugel <harpgamer31@gmail.com>
2024-03-04 22:42:10 -0300

    change navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,17 +4,17 @@ const kalam = Kalam({ weight: "400", subsets: ["latin"] });
 
 export function Navbar() {
   return (
-    <div className="md:flex md:items-center md:justify-between">
+    <div className="md:flex md:items-center md:justify-between md:fixed md:gap-[1421px]">
       <div className="md:pl-10">
         <a
           href="#"
-          className={`md:${kalam.className} md:text-lg md:font-normal md:text-primary md:ml-20`}
+          className={`${kalam.className} md:text-lg md:font-normal md:text-primary md:ml-20`}
         >
           OttoGugel
         </a>
       </div>
       <div className="md:flex md:gap-4 md:pr-10">
-        <a href="#" className="md:text-primary md:font-bold">
+        <a href="#" className="md:text-secondary md:font-normal md:hover:text-gray-50">
           Home
         </a>
         <a
@@ -26,7 +26,7 @@ export function Navbar() {
         <a href="#projects" className="md:text-secondary md:font-normal md:hover:text-gray-50">
           Projects
         </a>
-        <a href="#contact" className="text-secondary font-normal hover:text-gray-50">
+        <a href="#contact" className="md:text-secondary md:font-normal md:hover:text-gray-50">
           Contact
         </a>
       </div>
